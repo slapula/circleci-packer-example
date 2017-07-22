d001a9ef8eedfb6cb5d18f2fe5f1039889f694de
Aaron Smith <ajsmith10381@gmail.com>
2017-07-22 17:56:37 -0500

    changing push ro github script

--- ci/push_to_github.sh
+++ ci/push_to_github.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
-IMAGE_YML=manifest.json
+IMAGE_JSON=manifest.json
 ROOT_DIR=$(git rev-parse --show-toplevel)
 
 set -x
+
+cp "${IMAGE_JSON}" "${ROOT_DIR}"
 cd "${ROOT_DIR}"
 
-if ! git diff --quiet "packer/${IMAGE_YML}"; then
-  git add "packer/${IMAGE_YML}"
+if ! git diff --quiet "${IMAGE_JSON}"; then
+  git add "${IMAGE_JSON}"
   git commit -m "images: Updated by Packer [ci skip]"
   git push origin HEAD:${CIRCLE_BRANCH}
 else
-  echo "No changes for ${IMAGE_YML}."
+  echo "No changes for ${IMAGE_JSON}."
 fi
