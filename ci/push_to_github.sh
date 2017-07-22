8fd06d9594a1f6ccce9f0837569e593f98454079
Aaron Smith <ajsmith10381@gmail.com>
2017-07-22 17:40:13 -0500

    packer troubleshooting

--- ci/push_to_github.sh
+++ ci/push_to_github.sh
@@ -5,8 +5,8 @@ ROOT_DIR=$(git rev-parse --show-toplevel)
 set -x
 cd "${ROOT_DIR}"
 
-if ! git diff --quiet "${IMAGE_YML}"; then
-  git add "${IMAGE_YML}"
+if ! git diff --quiet "packer/${IMAGE_YML}"; then
+  git add "packer/${IMAGE_YML}"
   git commit -m "images: Updated by Packer [ci skip]"
   git push origin HEAD:${CIRCLE_BRANCH}
 else
